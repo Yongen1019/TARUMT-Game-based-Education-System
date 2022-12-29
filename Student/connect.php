0e5d0770958bc7043d2bd55713d1651c83d07287
yapyoonen <88510369+Yongen1019@users.noreply.github.com>
2022-12-29 22:49:38 +0800

    Delete Student directory

--- Student/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$DB_USER = "root";
-$DB_PASSWORD = "";
-$DB_HOST = "localhost";
-$DB_NAME = "TARUMTEducationDB";
-
-$db = new PDO("mysql:host=$DB_HOST; dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)
-?>
